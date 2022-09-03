91c74b3e5bc91d87c961b5da991552664f8bf578
DyoDevLmao <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-03 13:26:36 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -20,21 +20,21 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = 'https://telegra.ph/file/cce9ab4551f7150f1970d.jpg'
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : Yan Xiao
-${htjava} *✉️ Nama RL* : Rahasia🗿
+${htjava} *💌 Nama* : Ryu
+${htjava} *✉️ Nama RL* : DyoMarselo
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
-${htjava} *⏰ Tanggal lahir* : 15 Oktober 2004
+${htjava} *⏰ Tanggal lahir* : 20 February 2005
 ${htjava} *🎨 Umur* : 17
-${htjava} *🧮 Kelas* : 12
-${htjava} *🧩 Hobby* : Nonton Donghua, Chatting, Musik, Recode script bot
-${htjava} *💬 Sifat* : Idiot, Tidak Ramah, Bilek, Prik, Nolep
-${htjava} *🗺️ Tinggal* : Indo, Tulungagung, Jawa Timur
+${htjava} *🧮 Kelas* : 11
+${htjava} *🧩 Hobby* : Ngegame
+${htjava} *💬 Sifat* : Tidak Ramah, Bilek, Prik,
+${htjava} *🗺️ Tinggal* : Indo, madiun, Jawa Timur
 ${htjava} *❤️ Suka* : kucing
-${htjava} *💔 Benci* : autis, seleb
+${htjava} *💔 Benci* : autis, seleb,
 
 ${htjava} *📷 ɪɴsᴛᴀɢʀᴀᴍ* : ${sig}
-${htjava} *🇫  ғᴀᴄᴇʙᴏᴏᴋ* : Fay Cat's Kun
+${htjava} *🇫  ғᴀᴄᴇʙᴏᴏᴋ* : Mitsuha
 ${htjava} *🐈 ɢɪᴛʜᴜʙ:* ${sgh}
 •·––––––––––––––––––––––––––·•
 `
