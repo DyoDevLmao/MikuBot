a5af73c81a3e9dbe5e9890b718601433ed548df9
DyoDevLmao <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-03 18:44:45 +0700

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -14,9 +14,6 @@ handler.before = function (m, { isOwner, isAdmin, isBotAdmin }) {
 Warning: ${user.warning} / 10
 Jika warning mencapai 10 kamu akan *dibanned+kick*
 
-ketik #on antibadword* untuk menyalakan antibadword
-ketik #astagfirullah* atau *#maaf* untuk mengurangi warning
-
 “Barang siapa yang beriman kepada Allah dan Hari Akhir maka hendaklah dia berkata baik atau diam” (HR. al-Bukhari dan Muslim).`, wm, 'MATIKAN ANTIBADWORD', '.off antibadword', 'ASTAGHFIRULLAH', '.maaf', m)
         if (user.warning >= 10) {
             user.banned = true
