60ceb5fb298150716bfd34ce6417bd32fe486996
DyoDevLmao <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-04 10:08:30 +0700

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,15 +1,19 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
 let handler = async (m, { text }) => {
-  let user = global.db.data.users[m.sender]
-  user.afk = + new Date
-  user.afkReason = text
-  m.reply(`
-${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}
-`)
-}
+let name = m.pushName || conn.getName(m.sender)
+
+let user = global.db.data.users[m.sender]
+let wibu = `https://api.dhamzxploit.my.id/api/neko`    
+let thumb = await(await fetch(wibu)).buffer()
+user.afk = + new Date
+user.afkReason = text
+ conn.sendButtonDoc(m.chat, `${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}`, wm, 'ᴊᴀɴɢᴀɴ ᴅɪɢᴀɴɢɢᴜ ʏᴀ ᴋᴀᴋ', 'Bilek', m,  { contextInfo: { externalAdReply: { showAdAttribution: true,
+    }
+    } })
+            }
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
 handler.command = /^afk$/i
-handler.limit = false
 
 export default handler
-
