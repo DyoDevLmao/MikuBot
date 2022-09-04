3e16db3dc775ce1169361b64466f43172461c452
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-04 10:28:47 +0700

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -4,11 +4,9 @@ let handler = async (m, { text }) => {
 let name = m.pushName || conn.getName(m.sender)
 
 let user = global.db.data.users[m.sender]
-let wibu = `https://api.dhamzxploit.my.id/api/neko`    
-let thumb = await(await fetch(wibu)).buffer()
 user.afk = + new Date
 user.afkReason = text
- conn.sendButtonDoc(m.chat, `${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}`, wm, 'ᴊᴀɴɢᴀɴ ᴅɪɢᴀɴɢɢᴜ ʏᴀ ᴋᴀᴋ', 'Bilek', m,  { contextInfo: { externalAdReply: { showAdAttribution: true,
+ conn.sendButtonDoc(m.chat, `${conn.getName(m.sender)} is now AFK${text ? ': ' + text : ''}`, wm, 'ok', 'Bilek', m,  { contextInfo: { externalAdReply: { showAdAttribution: true,
     }
     } })
             }
