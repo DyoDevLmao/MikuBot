e0f8f542b027e8b7b01ed1f2e34ced4caa34cfeb
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-09 17:50:21 +0700

    Create cosplay.js

---
+++ plugins/cosplay.js
@@ -0,0 +1,10 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+  conn.sendFile(m.chat, global.API('adiisus', '/api/randomimage/cosplay'), 'cosplay.jpg', '_*Done*_', m)
+}
+handler.help = ['cosplay']
+handler.tags = ['internet']
+
+handler.command = /^(cosplay)$/i
+
+export default handler
