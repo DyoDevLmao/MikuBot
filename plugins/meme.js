69dc299626d3b51abff2aec6867059d938b239a8
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-09 18:06:17 +0700

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,11 +1,27 @@
+//created by Scooppt
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => {
-    let url = global.API('xteam', '/randomimage/meme', {}, 'APIKEY')
-    await conn.sendButtonImg(m.chat, url, '*MEME*', wm, 'NEXT', '.meme',m, 0, { thumbnail: await (await fetch(url)).buffer() })
+
+let handler  = async (m, { conn, text }) => {
+ try {
+    let res = await fetch('https://meme-api.herokuapp.com/gimme')
+    let json = await res.json()
+    if (json.status) throw json
+    let caption = `
+©Reddit
+Author: ${json.author} Subreddit: ${json.subreddit}
+${json.postLink}
+`.trim()
+    conn.sendFile(m.chat, json.url, 'test.jpg', caption, m)
+   } catch (e) {
+        console.log(e)
+        throw '_*Erro!*_'
+    }
 }
+
 handler.help = ['meme']
-handler.tags = ['fun']
-handler.command = /^(meme)$/i
-handler.limit = false
+handler.tags = ['random']
+handler.command = /^meme$/i
+
+handler.fail = null
 
 export default handler
