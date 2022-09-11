900c73c7a648e3891afa75d74052b0f9a43ffcf8
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-11 16:28:23 +0700

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -3,9 +3,12 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let res = await fetch('https://telegra.ph/file/08e57d12a311fcf78ebdf.jpg')
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Halo Kak >///<', 'Halo miku', 'iyh', m)
-}
+
+
+let info = `Elaina Disini (｡>_<｡)`
+
+
+
 
 handler.customPrefix = /^(tes|bot|miku)$/i
 
