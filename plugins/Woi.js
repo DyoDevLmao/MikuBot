c538cf76f95980f8072fd8b85ab831b11b464ce2
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-11 16:04:51 +0700

    Delete Woi.js

--- plugins/Woi.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch'
-
-let handler = async (m, { conn, text }) => {
-let res = await fetch('https://telegra.ph/file/15fad2458c84ab50913a3.jpg')
-
-handler.command = /^(loli)$/i
-handler.tags = ['anime']
-handler.help = ['loli']
-export default handler
