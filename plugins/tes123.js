39783fc3b6cd54d8f94b58e0a9840c6dd36312f1
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-11 16:33:03 +0700

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -4,13 +4,25 @@ import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 
-
-let info = `Elaina Disini (｡>_<｡)`
-
+let info = `Miku Disini (｡>_<｡)`
 
 
 
-handler.customPrefix = /^(tes|bot|miku)$/i
+let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+
+
+conn.reply(m.chat, info, m, { quoted: fkontak },{ contextInfo: { externalAdReply: { showAdAttribution: true,
+      mediaUrl: "https://Instagram.com/Xiao_yan_21",
+      mediaType: 2,
+      description: "https://Instagram.com/Xiao_yan_21", 
+      title: global.titlebot,
+      body: wm,
+      thumbnail: thumb,
+      sourceUrl: sig  }}})
+
+}
+
+handler.customPrefix = /^(tes|bot|Miku)$/i
 
 handler.command = new RegExp
 
