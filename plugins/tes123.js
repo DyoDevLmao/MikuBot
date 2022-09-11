b9ea12a15828c9a85f0bd08eb923f6c747042ef7
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-11 16:20:31 +0700

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -4,10 +4,7 @@ import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let res = await fetch('https://telegra.ph/file/08e57d12a311fcf78ebdf.jpg')
-if (!res.ok) throw await `${res.status} ${res.statusText}`;
-let json = await res.json();
-let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Halo Kak', 'Halo miku', 'iyh', m)
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Halo Kak >///<', 'Halo miku', 'iyh', m)
 }
 
 handler.customPrefix = /^(tes|bot|miku)$/i
