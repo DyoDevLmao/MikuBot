bb175093ff10099f607b4ada69818e5286a6b1e2
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-11 16:17:07 +0700

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -3,15 +3,11 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-
-let info = `Miku Disini (｡>_<｡)`
-let res = await fetch('https://telegra.ph/file/15fad2458c84ab50913a3.jpg')
-
-
-let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-
-
-
+let res = await fetch('https://telegra.ph/file/08e57d12a311fcf78ebdf.jpg')
+if (!res.ok) throw await `${res.status} ${res.statusText}`;
+let json = await res.json();
+let url = json[Math.floor(Math.random() * json.length)]
+await conn.sendButtonImg(m.chat, await (await fetch(url)).buffer(), 'Halo Kak', 'Halo miku', 'iyh', m)
 }
 
 handler.customPrefix = /^(tes|bot|miku)$/i
