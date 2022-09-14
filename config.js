8b4095de0b8a83aba224d90256cca4cccb843ba3
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-14 21:30:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
 // kalo ga punya ketik "-" atau biarin aja biar ada creditsnya :v
 global.sig = 'https://instagram.com/diavolo_fushiguro' //Link Instagrammu
 global.sgh = 'https://github.com/DyoDevLmao' //Link Githubmu
-global.sgc = '-'
+global.sgc = 'https://chat.whatsapp.com/DySMGRcq9odB1qVcZKyc9L'
 global.sdc = '-' //Isi Pake Link Discordmu Kalo Gada Biarin aja
 global.snh = 'https://nhentai.net/g/926291' //Hmmm, Ini boleh kalian isi terserah :v
 
