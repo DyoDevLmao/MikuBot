2c89429fe8a6f246f92960aba2c2d928e283ead7
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-21 17:15:43 +0700

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -1,10 +1,10 @@
 export function before(m) {
     let user = global.db.data.users[m.sender]
     if (user.afk > -1) {
-        conn.sendButton(m.chat,`
+        m.reply(`
   Kamu berhenti AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
   Selama ${(new Date - user.afk).toTimeString()}
-  `,wm,'Hai','Ya',m)
+  `.trim())
         user.afk = -1
         user.afkReason = ''
     }
@@ -17,11 +17,11 @@ export function before(m) {
         if (!afkTime || afkTime < 0)
             continue
         let reason = user.afkReason || ''
-        conn.sendButton(m.chat,`
+        m.reply(`
   Jangan tag dia!
   Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
   Selama ${(new Date - afkTime).toTimeString()}
-  `,wm,'Maaf','Ya',m)
+  `.trim())
     }
     return true
 }
