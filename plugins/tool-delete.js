38588667a4eba4f545c2befc0ecd438d7fbaedf3
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-23 19:21:06 +0700

    Update tool-delete.js

--- plugins/tool-delete.js
+++ plugins/tool-delete.js
@@ -2,7 +2,7 @@ let handler = function (m) {
     if (!m.quoted) throw false
     let { chat, fromMe, isBaileys } = m.quoted
     if (!fromMe) throw false
-    //if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
+    if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
     conn.sendMessage(chat, { delete: m.quoted.vM.key })
 }
 handler.help = ['del', 'delete']
