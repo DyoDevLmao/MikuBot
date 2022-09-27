170ac081fa2fa2087ffbad02efbee835944776a6
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-27 15:43:56 +0700

    Update cosplay.js

--- plugins/cosplay.js
+++ plugins/cosplay.js
@@ -1,10 +1,11 @@
 import fetch from 'node-fetch'
-let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, global.API('https://api.akuari.my.id/randomimganime/cosplay'), '_*Cosplayer*_', m)
+
+let handler = async (m, { conn, command }) => {
+	let url = 'https://api.akuari.my.id/randomimganime/cosplay'
+	conn.sendButton(m.chat, 'Cosplayer', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
-handler.help = ['cosplay']
-handler.tags = ['internet']
-
 handler.command = /^(cosplay)$/i
-
+handler.tags = ['internet']
+handler.help = ['cosplay']
+handler.limit = true
 export default handler
