d054de0315e15da7927427350e667a387b0eb2ce
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-27 13:49:47 +0700

    Update milf.js

--- plugins/milf.js
+++ plugins/milf.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, global.API('adiisus', '/api/randomimage/milf'), 'milf.jpg', '_*Nih Wibu*_', m)
+  conn.sendFile(m.chat, global.API('https://api.akuari.my.id/randomimganime/milf'), '_*Nih Wibu*_', m)
 }
 handler.help = ['milf']
 handler.tags = ['internet']
