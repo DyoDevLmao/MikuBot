c63780c695ba72e6c215871e84615b8b67c684d4
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-27 17:25:22 +0700

    Create school.js

---
+++ plugins/school.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command }) => {
+	let url = 'https://api.akuari.my.id/randomimganime/akaneko?param=masturbation'
+	conn.sendButton(m.chat, '🔞ahhhh>///<', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
+}
+handler.command = /^(school)$/i
+handler.tags = ['nsfw']
+handler.help = ['school']
+handler.limit = true
+export default handler
