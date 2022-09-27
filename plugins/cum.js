9b6e6d77af66a35887ac24f5f4461110a6e130ca
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-27 15:52:46 +0700

    Create cum.js

---
+++ plugins/cum.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command }) => {
+	let url = 'https://api.akuari.my.id/randomimganime/akaneko?param=cum'
+	conn.sendButton(m.chat, '🔞Dasar sangean', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
+}
+handler.command = /^(cum)$/i
+handler.tags = ['nsfw']
+handler.help = ['cum']
+handler.limit = true
+export default handler
