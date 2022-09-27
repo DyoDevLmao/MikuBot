0b06cba87e2bc968ce6225e0e7dbffb3ee8a5b3d
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-27 15:54:44 +0700

    Create pussy.js

---
+++ plugins/pussy.js
@@ -0,0 +1,11 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, command }) => {
+	let url = 'https://api.akuari.my.id/randomimganime/akaneko?param=pussy'
+	conn.sendButton(m.chat, '🔞Dasar sangean', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
+}
+handler.command = /^(pussy)$/i
+handler.tags = ['nsfw']
+handler.help = ['pussy']
+handler.limit = true
+export default handler
