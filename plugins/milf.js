b1a3399f02f627c7f9fdaf4cc8aba0d445c06787
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-09-27 15:42:37 +0700

    Update milf.js

--- plugins/milf.js
+++ plugins/milf.js
@@ -1,9 +1,11 @@
 import fetch from 'node-fetch'
-let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, global.API('https://api.akuari.my.id/randomimganime/milf'), '_*Nih Wibu*_', m)
+
+let handler = async (m, { conn, command }) => {
+	let url = 'https://api.akuari.my.id/randomimganime/milf'
+	conn.sendButton(m.chat, 'Bojone Gepenk', wm, await(await fetch(url)).buffer(), [['Next',`.${command}`]],m)
 }
-handler.help = ['milf']
-handler.tags = ['internet']
 handler.command = /^(milf)$/i
+handler.tags = ['anime']
+handler.help = ['milf']
 handler.limit = true
 export default handler
