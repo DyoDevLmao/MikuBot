f8fcbf87edb23218847f83ed692954cc4a355d16
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-10-05 14:19:48 +0700

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -1,33 +1,16 @@
-let handler = m => m
+let handler = async (m, { conn, text }) => {
+    let name = m.fromMe ? conn.user : conn.contacts[m.sender]
 
-let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
-
-handler.before = function (m, { isOwner, isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe && isOwner) return !0
-    let chat = db.data.chats[m.chat]
-    let user = db.data.users[m.sender]
-    let isBadword = badwordRegex.exec(m.text)
-
-    if (!chat.antiBadword && !chat.isBanned && !user.banned && isBadword && !isOwner && !isAdmin) {
-        user.warning += 1
-        this.send2Button(m.chat, `*badword terdeteksi!*
-Warning: ${user.warning} / 10
-Jika warning mencapai 10 kamu akan *dibanned+kick*
-
-ketik *.on antibadword* untuk menyalakan antibadword
-ketik *.maaf* untuk mengurangi warning
-
-“Jangan toxic!”, wm, 'MATIKAN ANTIBADWORD', '.off antibadword', 'ASTAGHFIRULLAH', '.maaf', m)
-        if (user.warning >= 10) {
-            user.banned = true
-            if (m.isGroup) {
-                if (isBotAdmin) {
-                	this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
-                   //this.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
-                }
-            }
-        }
-    }
-    return !0
+  conn.reply(m.chat, `
+*「 ANTI TOXIC 」*
+Pengirim : ${name.vnmae || name.notify || name.name || ('+' + name.jid.split`@`[0])}
+Pesan : ${m.text}
+Biasakan Jangan Toxic ya! :)
+`.trim(), m)
+    let mentionedJid = [m.sender]
 }
+handler.customPrefix = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
+
+handler.command = new RegExp
+
 Export default handler 
