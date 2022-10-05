aef4a41575e50b82f80864ea17d0a0f5325a7076
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-10-05 14:03:31 +0700

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -1,17 +1,30 @@
 let handler = m => m
 
-let linkRegex = /(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
-handler.before = function (m, { user }) {
-  if (m.isBaileys && m.fromMe) return true
-  if (/masuk|lanjutkan|banjir|(per)?panjang/g.exec(m.text)) return true
-  let chat = global.DATABASE.data.chats[m.chat]
-  let isGroupToxic = linkRegex.exec(m.text)
+let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
 
-  if (chat.antiToxic && isGroupToxic) {
-    m.reply('Jangan Toxic!!\n' + readMore + '\nMau Matikan? ketik */disable antitoxic*')
-    if (global.opts['restrict']) {
-      // if (!user.isAdmin) return true
-      // this.groupRemove(m.chat, [m.sender])
+handler.before = function (m, { isOwner, isAdmin, isBotAdmin }) {
+    if (m.isBaileys && m.fromMe && isOwner) return !0
+    let chat = db.data.chats[m.chat]
+    let user = db.data.users[m.sender]
+    let isBadword = badwordRegex.exec(m.text)
+
+    if (!chat.antiBadword && !chat.isBanned && !user.banned && isBadword && !isOwner && !isAdmin) {
+        user.warning += 1
+        this.send2Button(m.chat, `*Toxic terdeteksi terdeteksi!*
+Warning: ${user.warning} / 10
+Jika warning mencapai 10 kamu akan *dibanned+kick*
+
+
+“Jangan Toxic!".`, wm, 'MATIKAN ANTIBADWORD', '.off antibadword', '.maaf', m)
+        if (user.warning >= 10) {
+            user.banned = true
+            if (m.isGroup) {
+                if (isBotAdmin) {
+                	this.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+                   //this.groupSettingChange(m.chat, GroupSettingChange.messageSend, false)
+                }
+            }
+        }
     }
   }
   return true
