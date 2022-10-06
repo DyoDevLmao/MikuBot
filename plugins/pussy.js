48e542467640fa4527f33acf784b26dadc1a88c6
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-10-07 06:18:25 +0700

    Update pussy.js

--- plugins/pussy.js
+++ plugins/pussy.js
@@ -7,5 +7,6 @@ let handler = async (m, { conn, command }) => {
 handler.command = /^(pussy)$/i
 handler.tags = ['nsfw']
 handler.help = ['pussy']
+handler.admin = true
 handler.limit = true
 export default handler
