c47763b70eaf1bd3125c5b5b8beb0d3585e2f2ec
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-10-10 16:41:49 +0700

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -1,3 +1,30 @@
+/*
+
+let handler = m => m
+
+let linkRegex = /(a(su|nj(([ie])ng|([ie])r)?)|me?me?k|ko?nto?l|ba?bi|fu?ck|ta(e|i)k|bangsat|g([iueo])bl([iueo])(k|g)|g ([iueo]) b l ([iueo]) (k|g)|a (n j (i n g|i r)?)s u|col(i|ay)|an?jg|b([ia])ngs([ia])?t|t([iuo])l([iuo])l)/i
+handler.before = function (m, { user }) {
+  if (m.isBaileys && m.fromMe) return true
+  if (/masuk|lanjutkan|banjir|(per)?panjang/g.exec(m.text)) return true
+  let chat = global.DATABASE.data.chats[m.chat]
+  let isGroupToxic = linkRegex.exec(m.text)
+
+  if (chat.antiToxic && isGroupToxic) {
+    m.reply('Jangan Toxic ya!!\n' + readMore + '\nMau Matikan? ketik * /disable antitoxic*')
+    if (global.opts['restrict']) {
+      // if (!user.isAdmin) return true
+      // this.groupRemove(m.chat, [m.sender])
+    }
+  }
+  return true
+}
+
+export const disable = true
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+*/
+
 const isToxic = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole|a(su|sw|syu)/i // tambahin sendiri
 
 export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin }) {
@@ -11,7 +38,7 @@ export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBo
     let bang = m.key.id
     
     if (chat.antiToxic && isAntiToxic) {
-        await conn.reply(m.chat, *Kata Toxic Terdeteksi!*, m)
+        await conn.sendButton(m.chat, `*Kata Aneh Terdeteksi!* ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, author, ['off antitoxic', '/disable antitoxic'], m)
         if (isBotAdmin && bot.restrict) {
             // await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
     global.db.data.users[m.sender].warn += 1
