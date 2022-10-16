64d17df95bdae54323e7c687fad3bb57812dc4fd
DyoDev <109974642+DyoDevLmao@users.noreply.github.com>
2022-10-16 15:33:49 +0700

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -1,4 +1,4 @@
-/*
+
 
 let handler = m => m
 
@@ -23,25 +23,3 @@ export const disable = true
 
 const more = String.fromCharCode(8206)
 const readMore = more.repeat(4001)
-*/
-
-const isToxic = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|meki|titi(t|d)|pe?ler|tetek|toket|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|bego|dajj?al|janc(u|o)k|pantek|puki ?(mak)?|kimak|kampang|lonte|col(i|mek?)|pelacur|henceu?t|nigga|fuck|dick|bitch|tits|bastard|asshole|a(su|sw|syu)/i // tambahin sendiri
-
-export async function before(m, { conn, args, usedPrefix, command, isAdmin, isBotAdmin }) {
-    if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let chat = global.db.data.chats[m.chat]
-    let bot = global.db.data.settings[this.user.jid] || {}
-    const isAntiToxic = isToxic.exec(m.text)
-    let hapus = m.key.participant
-    let bang = m.key.id
-    
-    if (chat.antiToxic && isAntiToxic) {
-        await conn.sendButton(m.chat, `*Kata Toxic Terdeteksi!* ${isBotAdmin ? '' : '\n\n_Bot bukan atmin_'}`, author, ['off antitoxic', '/disable antibadword'], m)
-        if (isBotAdmin && bot.restrict) {
-    }
-    return !0
-}
-
-export const disable = true
